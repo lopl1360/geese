32ebae990e338fd1dd65e6a7bcebb2fb85a31fb2
pezhman <lopl@lopl.net>
2018-10-23 11:51:58 -0700

    Models: Add a new model

--- geese/models.py
+++ geese/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class url(models.Model):
+    url = models.TextField(null=False)
+    short = models.CharField(max_length=32)
+    count = models.IntegerField(default=0)
