d9b8d00cfe2de0064dad98b09d54969bb2dca5b6
pezhman <lopl@lopl.net>
2018-10-23 19:21:50 -0700

    Handle Post request in geese view

--- geese/views.py
+++ geese/views.py
@@ -2,9 +2,33 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+@csrf_exempt
 def index(request):
-    return HttpResponse("hello world...........")
+    if request.method == 'POST':
+	return handlePost(request)
+    else:
+	return handleGet(request)
 
+
+
+def handlePost(request):
+	response = {}
+	try:
+		url = request.POST['URL']
+		short = makeItShort(url)
+		response = {'status': 'ok', 'short': short}
+	except Exception as e:
+		response = {'status' : 'failed', 'error': str(e)}
+
+	return JsonResponse(response);
+
+def handleGet(request):
+	return HttpResponse("hello world...........")
+
+def makeItShort(url):
+	return "abcdefghi"
